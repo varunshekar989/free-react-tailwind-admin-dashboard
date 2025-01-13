7ecba3e145f17670f57a98030d023d78e90e834d
harish-m-rezoy <harish.m@rezoy.app>
2025-01-13 21:21:04 +0530

    fix: correct "Total views" to "Total view" in ECommerce component and add Tailwind CSS configuration

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -11,7 +11,7 @@ const ECommerce: React.FC = () => {
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardDataStats title="Total views" total="$3.456K" rate="0.43%" levelUp>
+        <CardDataStats title="Total view" total="$3.456K" rate="0.43%" levelUp>
           <svg
             className="fill-primary dark:fill-white"
             width="22"
